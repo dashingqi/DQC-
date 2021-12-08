df34709d225570ff110632571fc643829a22a74d
zhangqi61 <zhangqi61@baidu.com>
2021-12-08 10:54:57 +0800

    Initial Commit

---
+++ C++Project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  C++Project
+//
+//  Created by zhangqi61 on 2021/12/8.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
