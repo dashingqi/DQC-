72239571daa943d516983423529d10b5e3f3b50f
zhangqi61 <zhangqi61@baidu.com>
2021-12-11 10:29:56 +0800

    [init]

--- C++Project/main.cpp
+++ C++Project/main.cpp
@@ -5,10 +5,15 @@
 //  Created by zhangqi61 on 2021/12/8.
 //
 
+// cout 和 endl 是在头文件iostream中定义的，并且位于命名空间 std中；
 #include <iostream>
-
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    using namespace std;
+    // \n 也是能用于换行
+    cout << "Hello, World!\n";
+    cout << "String";
+    // 进行换行
+    cout << endl;
+    
     return 0;
 }
